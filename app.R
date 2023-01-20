03b8cfc711241fb199a3dca0ef4888d29041b890
ttnsy <tanesya.t@gmail.com>
2023-01-20 18:19:40 +0700

    chore: add appsilon logo & contact

--- app.R
+++ app.R
@@ -16,14 +16,26 @@ ui <- div(
   tags$script(src = "https://cdn.tailwindcss.com"),
   tags$script(src = "main.js"),
   div(
-    class = "mb-2 p-1.5",
-    span(
-      class = "text-lg",
-      "🐝",
+    class = "flex items-center justify-between mr-8 mb-2 p-1.5",
+    div(
+      class = "flex items-center",
+      tags$a(
+        href = "https://appsilon.com/",
+        target = "blank",
+        img(src = "appsilon-logo.png", style = "width: 150px")
+      ),
       span(
-        class = "font-bold",
-        " Bee Colonies"
+        class = "text-lg ml-2",
+        " 🐝",
+        span(
+          class = "font-bold",
+          " Bee Colonies"
+        )
       )
+    ),
+    div(
+      class = "bg-white hover:bg-yellow-400 text-sm font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded shadow",
+      tags$a(href = "https://appsilon.com/#contact", "Let's Talk")
     )
   ),
   div(
